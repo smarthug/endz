760f1ffeec7afaa36a5dccfb1dd0b8cb969649aa
smarthug <kirklayer@gmail.com>
2019-12-07 00:12:28 +0900

    typed js impl

--- README.md
+++ README.md
@@ -5,17 +5,21 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 - [ ] 우선은 읽는 사람 기준 프런트 웹 사이트 생성
 - [ ] 그 후 작가 기준 웹 프런트 생성
 
+
+## 중요한 링크
+- [ ] https://github.com/mattboldt/typed.js/
+
 ## Useful Links
 - [ ] https://github.com/nashkenazy/React-Visual-Novel
 - [ ] https://github.com/syncopika/visual_novel_maker
  - [ ] https://github.com/wutongjie23hao/novel2video
  - [ ] https://github.com/open-novel/Player
- - [ ] getting DB by crawling
  - [ ] https://github.com/8novels/jk-meets-js
  - [ ] https://github.com/8novels
  - [ ] https://github.com/vkbo/novelWriter
  - [ ] https://github.com/tategakibunko/TypeNovel
  - [ ] https://github.com/YouYII/EasyReader
+ - [ ] https://github.com/IllgamhoDuck/ko_novel_generator
 
 
 ## Design Reference
--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Typed from 'typed.js';
+
+import useTyped from 'use-typed'
+
+
+var options = {
+  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+  typeSpeed: 40
+};
 
 function App() {
+
+  const hRef = useRef(null)
+  useTyped(hRef, {
+    strings: [
+      'dynamic-novel first commit',
+      'now Story begins',
+      'hold on tight'
+    ],
+    typeSpeed: 30,
+    backSpeed: 30
+  });
+
+  // useEffect(()=>{
+  //   const options = {
+  //     strings: 'Hello , world!',
+  //     typeSpeed: 50,
+  //     backSpeed:50
+
+  //   }
+
+  //  new Typed(hRef.current, options)
+
+  // }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-       <h1>dynamic-novel first commit</h1>
+       <div ref={hRef}>dynamic-novel first commit</div>
       </header>
     </div>
   );
