8bab42854f43d3d7cc0a501932af7f0db8358052
smarthug <kirklayer@gmail.com>
2019-12-20 00:13:04 +0900

    framework init

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,50 +1,63 @@
 import React, { useRef, useEffect } from "react";
-import ReactDOM from "react-dom";
 import Typed from "typed.js";
 
-import {FixedSizeList} from 'react-window'
-
-import Para from './paragraph'
+import {data} from '../../global'
 
 export default function Main() {
-  const textRef = useRef(null);
-  const sampleRef = useRef(null);
+    const textRef = useRef(null);
+    const sampleRef = useRef(null);
 
-  useEffect(() => {
-    
-      //console.log(sampleRef.current)
-    // var options = {
-    //   stringsElement: sampleRef.current,
-    //   typeSpeed: 70,
-    //   backSpeed: 0,
-    //   onComplete : (self) => {
-    //     console.log(self);
-    //   }
+    useEffect(() => {
+        Next();
+
       
-    // };
+    }, []);
 
-    //const typed = new Typed(textRef.current, options);
-    //typed.typeSpeed = 0.01;
-    //typed.destroy();
-    //console.log(s)
-  }, []);
+    // 인자로 페이지나 다음으로 갈곳에 대한 객체를 받고 다음 씬을 추가 .. 
+    function Next() {
+        console.log("Next");
 
-  function Skip() {
-      console.log("skip");
-      //textRef.current.typeSpeed = 0;
-  }
+       
+        var b = document.createElement("img")
+        b.src = "https://cpb-us-w2.wpmucdn.com/blogs.baylor.edu/dist/c/3127/files/2015/12/DQ-Screen-Shot-2015-12-04-at-4.10.29-PM-29wbxtz.png"
+        b.width = window.innerWidth;
+        sampleRef.current.appendChild(b)
+       
 
-  return (
-    <div className="App">
-     
-      <h1>Don quixote</h1>
-      <FixedSizeList
-       height={900}
-       itemCount={100}
-       itemSize={1000}
-       width={"100%"}>
-          {Para}
-      </FixedSizeList>
-    </div>
-  );
+        
+
+        var d = document.createElement("div")
+        sampleRef.current.appendChild(d)
+       
+
+        var options = {
+            strings: [data.pages[0].text],
+            typeSpeed: 2,
+            backSpeed: 0,
+            onComplete: (self) => {
+                console.log(self);
+                Next();
+            },
+            loop: false
+
+        };
+
+        const typed = new Typed(d, options);
+        
+
+        
+
+
+    }
+
+    return (
+        <div ref={sampleRef} className="App">
+            
+
+            <h1>Don quixote</h1>
+
+           
+
+        </div>
+    );
 }
