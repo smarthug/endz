b49483f17b72057c009c9f7a619b353ca865f647
smarthug <kirklayer@gmail.com>
2020-01-02 15:11:06 +0900

    운수좋은날 2020 init

--- src/Pages/Test/index.js
+++ src/Pages/Test/index.js
@@ -4,72 +4,26 @@ import './customSwiper.css'
 
 import { data } from '../../global'
 
-// import { Swiper, Slide } from 'react-dynamic-swiper'
-//  import 'react-dynamic-swiper/lib/styles.css'
+import { Plugins } from '../../Plugins'
 
-import { makeStyles } from '@material-ui/core/styles'
 
-var slideCount = 5;
 
-var options = {
-    navigation: true,
-    pagination: true,
-    paginationClickable: true,
-    scrollBar: false,
-    loop: false
-}
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        height: 100
-    }
-}))
 
-// export default function Test() {
 
-//     const classes = useStyles()
-
-//     return (
-//         <div className="Demo">
-//             <div className="Demo-swiper">
-
-//                 <Swiper
-//                     swiperOptions={{ scrollbarHide: false }}
-//                     {...options}
-//                 >
-//                     {(new Array(slideCount).fill(null).map((_, i) => (
-//                         <Slide className="Demo-swiper__slide" key={i}>
-//                             Slide {i + 1}
-//                         </Slide>
-//                     )))}
-//                 </Swiper>
-//             </div>
-
-//         </div >
-//     )
-// }
-
-// pages 배열은 글로벌에서 오고 ... 파서함수로 다 돌려줄까 ....
 
 
 
 export default function Test() {
 
-    const classes = useStyles()
+   
 
     useEffect(() => {
         var mySwiper = new Swiper('.swiper-container', {
             // Optional parameters
             direction: 'horizontal',
             loop: false,
-            // navigation: false,
 
-            // // If we need pagination
-            /// pagination: {
-            //     el: '.swiper-pagination',
-            // },
-
-            // // Navigation arrows
             navigation: {
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
@@ -79,6 +33,8 @@ export default function Test() {
             scrollbar: {
                 el: '.swiper-scrollbar',
             },
+
+            
         })
     }, [])
 
@@ -88,14 +44,14 @@ export default function Test() {
 
             <div className="swiper-wrapper">
 
-                <div className="swiper-slide">Slide 1</div>
-                <div className="swiper-slide">Slide 2</div>
-                <div className="swiper-slide">Slide 3</div>
+
 
 
                 {data.pages.map((v, i) => {
+                    var ReactComponent = Plugins[data.pages[0].type]
+                    console.log(ReactComponent)
                     return (
-                        <div className="swiper-slide">{v.text}</div>
+                    <div className="swiper-slide"><ReactComponent v={v}/></div>
                     )
                 })}
             </div>
--- src/Plugins/text.js
+++ src/Plugins/text.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import { data } from '../global'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+   swiperSlide: {
+      fontSize: "1rem",
+      letterSpacing: "normal",
+      lineHeight: "2rem",
+      whiteSpace: "pre-line"
+   }
+}))
 
 
-export default function TextPage(v){
+export default function TextPage(props) {
+   const classes = useStyles()
 
+
+   return (
+     
+         <div className={classes.swiperSlide}>
+           {props.v.text}
+         </div>
     
-
-     return (
-        <div className="swiper-slide">{v.text}</div>
-     )
+   )
 
 }
+
