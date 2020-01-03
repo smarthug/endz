408a6abdc0d0dfcc0c956a54b6b025988d2662fd
smarthug <kirklayer@gmail.com>
2020-01-03 20:34:49 +0900

    antialias

--- src/Plugins/threeScene.js
+++ src/Plugins/threeScene.js
@@ -14,7 +14,7 @@ export default function ThreePage(props) {
         scene = new THREE.Scene();
         camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-        renderer = new THREE.WebGLRenderer();
+        renderer = new THREE.WebGLRenderer({antialias:true});
         renderer.setClearColor(0xffffff, 1)
         renderer.setSize(window.innerWidth, window.innerHeight);
         threeRef.current.appendChild(renderer.domElement);
